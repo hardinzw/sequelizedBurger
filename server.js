ee33b500d87a4df8cb098e8bb0ba0c47681c36ef
Zachary Hardin <hardinzw@gmail.com>
2019-08-29 13:02:21 -0400

    rearranged folders

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
+var bodyParser = require("body-parser");
+//var methodOverride = require("method-override");
 
 var app = express();
 
+var db = require("./models");
+
+// // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// // override with POST having ?_method=DELETE
+//app.use(methodOverride("_method"));
 
-// Set Handlebars.
+// initiate handlebars with default layout
 var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine(
+	"handlebars",
+	exphbs({
+		defaultLayout: "main"
+	})
+);
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./app/controllers/burgers_controller.js");
+// establish api routes for database access
+require("./routes/api-routes")(app);
 
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+var port = process.env.PORT || 3000;
+// connect to database, sync with database, then listen on port 3000
+db.sequelize.sync().then(() => {
+	app.listen(port, () => {
+		console.log(`Listening on port ${port}`);
+	});
 });
